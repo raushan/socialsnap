1e7cceb1e0028ba4351a5b301b242d875842731a
Raushan Alleyne <raushan802@gmail.com>
2014-11-22 23:21:50 -0500

    setting up google maps api, adding placeholder views for gallery images

---
+++ src/com/cmsc436/socialsnap/GalleryViewActivity.java
@@ -0,0 +1,18 @@
+package com.cmsc436.socialsnap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GalleryViewActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    
+	    
+	    
+	   
+	}
+
+}
